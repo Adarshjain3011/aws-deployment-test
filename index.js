6255110b279121d3ded63e5ae9ae5db2f551283f
Adarshjain3011 <adarshjain3011@gmail.com>
2026-06-06 17:37:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const express = require("express");
+
+const app = express();
+
+
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+
+    console.log("server listen on the port 3000");
+
+})
+
+app.get("/home",(req,res)=>{
+
+    res.send("<h1>hellow world </h1>")
+});
+
+
+
